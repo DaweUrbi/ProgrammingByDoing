be5a3a90ce2c6de77be11678b706171e2692ee57
DaweUrbi <urban.da@icloud.com>
2022-02-16 11:23:42 -0800

    Finished

--- src/week1/classes/Driver.java
+++ src/week1/classes/Driver.java
@@ -1,4 +1,20 @@
 package week1.classes;
 
 public class Driver {
+    public static void main(String[] args) {
+
+        Rectangle r1 = new Rectangle(5, 7);
+        Rectangle r2 = new Rectangle(10, 2, "Green");
+        Rectangle r3 = new Rectangle(15, 12, "Red");
+        r1.draw();
+        System.out.print("\n");
+        r2.draw();
+        System.out.print("\n");
+        r3.draw();
+        r1.setHeight(15);
+        r1.setWidth(20);
+        r1.setColor("White");
+        r1.draw();
+    }
+
 }
--- src/week1/classes/Rectangle.java
+++ src/week1/classes/Rectangle.java
@@ -1,36 +1,79 @@
 package week1.classes;
 
+import java.util.Locale;
+
 public class Rectangle {
     // fields
-    int width;
-    int height;
-    String color;
+    private int width;
+    private int height;
+    private String color;
 
-    Rectangle() {
-        this.width = 1;
-        this.height = 1;
-        this.color = Blue;
+
+    // constructor
+    public Rectangle(int width, int height) {
+        if (width == 0) {
+            width = 1;
+        }
+        this.width = width;
+
+        if (height == 0) {
+            height = 1;
+        }
+        this.height = height;
+        color = "Blue";
     }
 
-    public void setColor(String color){
+    public Rectangle(int width, int height, String color) {
+        if (width == 0) {
+            width = 1;
+        }
+        this.width = width;
+
+        if (height == 0) {
+            height = 1;
+        }
+        this.height = height;
+
         if (color.length() > 2 && color.length() <= 20) {
-            this.color = color;
+            color = color.substring(0, 1).toUpperCase() + color.substring(1);
+        } else {
+            color = "Blue";
+        }
+        this.color = color;
+    }
+
+    // Draw function
+    public void draw() {
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                System.out.print(color.substring(0, 1));
+            }
+            System.out.println("");
+        }
+    }
+    // setter
+    public void setWidth(int width) {
+        if (width == 0) {
+            this.width = 1;
+        } else {
+            this.width = width;
         }
     }
 
-    Rectangle(int newWidth, int newHeight, String newColor) {
-        width = newWidth;
-        height = newHeight;
-        color = newColor;
+    public void setHeight(int height) {
+        if (height == 0) {
+            this.height = 1;
+        } else {
+            this.height = height;
+        }
     }
 
-    void draw()
-
-
-
-
-
+    public void setColor(String color) {
+        if (color.length() > 2 && color.length() <= 20) {
+            color = color.substring(0, 1).toUpperCase() + color.substring(1);
+        } else {
+            color = "Blue";
+        }
+        this.color = color;
+    }
 }
-
-
-
